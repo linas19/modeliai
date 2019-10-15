b6b9a3821da219d1d07ba5e10c7be12c6b0b6b03
Linas <34280196+linas19@users.noreply.github.com>
2019-10-15 13:37:27 +0300

    Update statistiniai modeliai.py

--- statistiniai modeliai.py
+++ statistiniai modeliai.py
@@ -1,7 +1,12 @@
-
-
+#Zemiau pateiktas linijines regresijos modelis
 model_linear_regression = LinearRegression()
 result_linear_regression = model_linear_regression.fit(X_train, y_train)
 
-model = LogisticRegression()
-result = model.fit(X_train, y_train)
+#Zemiau pateiktas logistines regresijos modelis
+model_logistic_regression = LogisticRegression()
+result_logistic_regression = model_logistic_regression.fit(X_train, y_train)
+
+#Zemiau pateiktas sprendimu medzio modelis
+model_classification_tree = tree.DecisionTreeClassifier(max_depth=6)
+result_classification_tree = model_classification_tree.fit(X_train, y_train)
+
